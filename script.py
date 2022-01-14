68666234c25440f8ba433c9049e7c142d458be6c
Atharva Parikh <54139853+AtharvaParikh@users.noreply.github.com>
2022-01-14 12:14:12 +0530

    viewing tables

--- script.py
+++ script.py
@@ -8,11 +8,13 @@ import numpy as np
 st.title('Smart India Hackathon 2022')
 
 st.markdown("""
-This app retrieves the list of the **Problem statements** from 
+This app retrieves the list of the **Problem statements** from sih website
 * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
 * **Data source:** [SIH website](https://www.sih.gov.in/sih2022PS).
 """)
-
+# https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
+# https://www.sih.gov.in/partner
+# https://www.sih.gov.in/sih2022PS
 st.sidebar.header('Filters')
 
 @st.cache
@@ -20,8 +22,10 @@ def load_data():
     url = 'https://www.sih.gov.in/sih2022PS'
     html = pd.read_html(url, header = 0)
     print(len(html))
-    df = html[0]
-    return df
+    df1,df2,df3 = html
+    return df1,df2,df3
 
-df = load_data()
-df
+url = 'https://www.sih.gov.in/sih2022PS'
+html = pd.read_html(url, header = 0)
+html
+st.write("Total entries - ", len(html))
