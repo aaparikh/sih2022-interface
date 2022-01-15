ad41cf3cf03a5b9e23aad8ab4ad476d12851a4eb
Atharva Parikh <54139853+AtharvaParikh@users.noreply.github.com>
2022-01-15 23:48:13 +0530

    working code 🥲

--- script.py
+++ script.py
@@ -4,6 +4,8 @@ import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from bs4 import BeautifulSoup
+import requests
 
 st.title('Smart India Hackathon 2022')
 
@@ -12,20 +14,64 @@ This app retrieves the list of the **Problem statements** from sih website
 * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
 * **Data source:** [SIH website](https://www.sih.gov.in/sih2022PS).
 """)
-# https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
-# https://www.sih.gov.in/partner
-# https://www.sih.gov.in/sih2022PS
+
 st.sidebar.header('Filters')
 
 @st.cache
 def load_data():
-    url = 'https://www.sih.gov.in/sih2022PS'
-    html = pd.read_html(url, header = 0)
-    print(len(html))
-    df1,df2,df3 = html
-    return df1,df2,df3
+    url = "https://sih.gov.in/sih2022PS"
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, "lxml")
 
-url = 'https://www.sih.gov.in/sih2022PS'
-html = pd.read_html(url, header = 0)
-html
-st.write("Total entries - ", len(html))
+    # titles of resulting table
+    titles = [
+        "Organization",
+        "Description",
+        "Category",
+        "Domain Bucket",
+        "Youtube Link",
+        "Dataset Link",
+        "PS No.",
+        "Submitted Idea Count"
+    ]
+
+    table = soup.find("table", {"id":"dataTablePS"})
+    #recursive = False is needed to avoid reading the inner tr tags
+    table_data = table.tbody.find_all("tr",recursive=False)
+    print("Total rows are - ", len(table_data))
+    data = []
+    for i,row in enumerate(table_data):
+        each_row = []
+        table_start = row.find_all("td", {"class":"colomn_border"}, recursive=False)
+        #need to dig deep in second item
+        inner_table = table_start[1].find("table", {"id":"settings"})
+        inner_table_rows = inner_table.thead.find_all("tr")
+        #now seperately extract the data from inner table
+        inner_row1 = inner_table_rows[0].find("div",{"class":"style-2"})
+        description = inner_row1.text.strip()
+        organization = inner_table_rows[1].td.text.strip()
+        category = inner_table_rows[2].td.text.strip()
+        domain_bucket = inner_table_rows[3].td.text.strip()
+        yt_link = inner_table_rows[4].td.a.text.strip() if inner_table_rows[4].td.a.text!="" else "NA"
+        dataset_link = inner_table_rows[5].td.a.text.strip() if inner_table_rows[5].td.a else "NA"
+        tds = row.find_all("td")
+        #from the outer table we need last third and last second field values
+        ps_number,submitted_idea_count = tds[-3].text.strip(), tds[-2].text.strip()
+        each_row.append(organization)
+        each_row.append(description)
+        each_row.append(category)
+        each_row.append(domain_bucket)
+        each_row.append(yt_link)
+        each_row.append(dataset_link)
+        each_row.append(ps_number)
+        each_row.append(submitted_idea_count)
+        # print(each_row)
+        data.append(each_row)
+        print(f"Row number {i} done")
+
+    df = pd.DataFrame(data, columns=titles)
+    # df.to_csv("sih2022PS.csv", index=False)
+    print(df.head())
+    return df
+
+df = load_data()
